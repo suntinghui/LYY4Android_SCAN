599d0956f93f5729240ec84a9c7b44788813fdff
liying <15901459407@163.com>
2014-09-29 20:42:09 +0800

    扫描成功界面的修改 添加完成按钮 和扫描界面与完成界面的值传递

--- src/com/people/lyy/scan/activity/SuccessActivity.java
+++ src/com/people/lyy/scan/activity/SuccessActivity.java
@@ -7,11 +7,14 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class SuccessActivity extends BaseActivity {
 	private TextView tv_cost = null;
-
+	private Button btn_over = null;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -23,10 +26,22 @@ public class SuccessActivity extends BaseActivity {
 
 	public void initview() {
 		tv_cost = (TextView) findViewById(R.id.tv_cost);
+		btn_over = (Button) findViewById(R.id.btn_over);
 	}
 
 	public void setview() {
-		tv_cost.setText(Constants.COST + "");
+		Intent intent = getIntent();
+		String reponse = intent.getStringExtra("result"); 
+		tv_cost.setText(reponse);
+		btn_over.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				finish();
+				
+			}
+		});
+		
 	}
 
 	@Override
