87ac9906102b338699e6bbab16e538ce81cf4ab1
suntinghui <tinghuisun@163.com>
2014-10-07 20:35:28 +0800

    STH

--- src/com/people/lyy/scan/activity/BarCodeActivity.java
+++ src/com/people/lyy/scan/activity/BarCodeActivity.java
@@ -108,16 +108,4 @@ public class BarCodeActivity extends BaseActivity {
 		}
 	}
 
-	private static float distance(MotionEvent event) {
-		float dx = event.getX(1) - event.getX(0);
-		float dy = event.getY(1) - event.getY(0);
-		return FloatMath.sqrt(dx * dx + dy * dy);
-	}
-
-	private static PointF mid(MotionEvent event) {
-		float midx = event.getX(1) + event.getX(0);
-		float midy = event.getY(1) - event.getY(0);
-
-		return new PointF(midx / 2, midy / 2);
-	}
 }
